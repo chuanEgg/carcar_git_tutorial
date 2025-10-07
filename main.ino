62bd5621875f2f7787d0749fe22ecc1783f29b8d
chuan <b12901049@ntu.edu.tw>
2025-10-07 20:49:55 +0800

    add workflow for arduino

---
+++ main.ino
@@ -0,0 +1,11 @@
+// CarCar Arduino Tutorial POC
+// Task: Print "Hello, World!" to the serial monitor once after startup.
+
+void setup() {
+  Serial.begin(9600);
+  // TODO: Print "Hello, World!" below
+}
+
+void loop() {
+  // Leave empty for this assignment
+}
